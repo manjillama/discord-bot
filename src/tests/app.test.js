6b024dc739c92464ff71e13efd98a731c7dc8be2
Manjil Tamang <lamamanjil@gmail.com>
2020-07-21 12:29:59 +0545

    supertest leakage fixed

--- src/tests/app.test.js
+++ src/tests/app.test.js
@@ -2,8 +2,9 @@ const request = require('supertest');
 const app = require('../app');
 
 describe('Test the root path', () => {
-  test('Server status test', async () => {
+  test('Server status test', async done => {
     const response = await request(app).get('/');
     expect(response.statusCode).toBe(200);
+    done();
   });
 });
